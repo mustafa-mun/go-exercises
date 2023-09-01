b4c55ebe8a8c016096b72f9897310a3dbd18f5a0
mustafa <mustafamcm9@gmail.com>
2023-09-01 22:08:37 +0300

    create the main loop

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,23 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/mustafa-mun/go-exercises/quiz-game/internal/handlecsv"
 )
 
+
 func main() {
+	csvPtr := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
+	limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 
-    csvPtr := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
-    limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds")
-  
-    flag.Parse()
+	flag.Parse()
 
-		csv := *csvPtr
-		handlecsv.ReadCsvFile(csv)
-		// limit := *limitPtr
-    fmt.Println("csv:", *csvPtr)
-    fmt.Println("limit:", *limitPtr)
-		
+	csv := *csvPtr
+	data, err := handlecsv.ReadCsvFile(csv)
+
+	if err != nil {
+		panic(err)
+	}
+	// limit := *limitPtr
+	fmt.Println("csv:", *csvPtr)
+	fmt.Println("limit:", *limitPtr)
+
+	var questionNumber int = 1
+	var pointer int = 0
+	var correctAnswerCount = 0
+	var examLength string = strconv.Itoa(len(data))
+	reader := bufio.NewScanner(os.Stdin)
+	
+	questionNumberString := strconv.Itoa(questionNumber)
+	printPrompt("Quesion #"+ questionNumberString + " "+ data[pointer][0])
+	// Begin the repl loop
+	for reader.Scan() {
+		if pointer == len(data) - 1 { // Stop the loop if user answers all questions correctly
+			fmt.Println("You scored", examLength, "out of", examLength, "questions.")
+			return
+		}
+		text := cleanInput(reader.Text()) // Get the input
+		answer := data[pointer][1] 
+		// Check if answer is correct
+		if text != answer {
+			// Answer is not correct 
+			fmt.Println("You scored", strconv.Itoa(correctAnswerCount), "out of", examLength, "questions.")
+			return
+		}
+		// Answer is correct
+		correctAnswerCount += 1
+		pointer += 1
+		questionNumber += 1 
+		printPrompt("Quesion #"+ strconv.Itoa(questionNumber) + " "+ data[pointer][0])
+	}
+	// Print an additional line if we encountered an EOF character
+	fmt.Println()
 	
 }
