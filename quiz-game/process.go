c5054f978a77eba518b8ad6a172acae98b952781
mustafa <mustafamcm9@gmail.com>
2023-09-04 19:31:30 +0300

    process file   This is for repl loop function

---
+++ quiz-game/process.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+)
+
+
+func processInput(data [][]string, wg *sync.WaitGroup, correctAnswerCount *int) {
+	defer wg.Done() // Close the waitgroup when the repl loop is over
+	
+	var questionNumber int = 1
+	var pointer int = 0
+	var examLength string = strconv.Itoa(len(data))
+	var reader *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+	questionNumberString := strconv.Itoa(questionNumber)
+	printPrompt("Quesion #"+ questionNumberString + " "+ data[pointer][0])
+	// Begin the repl loop
+
+	for reader.Scan() {
+		if pointer == len(data) - 1 { // Stop the loop if user answers all questions correctly
+			fmt.Println("You scored", examLength, "out of", examLength, "questions.")
+			return
+		}
+		text := cleanInput(reader.Text()) // Get the input
+		answer := data[pointer][1] 
+		// Check if answer is correct
+		if text != answer {
+			// Answer is not correct 
+			fmt.Println("You scored", strconv.Itoa(*correctAnswerCount), "out of", examLength, "questions.")
+			return
+		}
+		// Answer is correct
+		*correctAnswerCount += 1
+		pointer += 1
+		questionNumber += 1 
+		printPrompt("Quesion #"+ strconv.Itoa(questionNumber) + " "+ data[pointer][0])
+
+	}
+	
+	// Print an additional line if we encountered an EOF character
+	fmt.Println()
+}
