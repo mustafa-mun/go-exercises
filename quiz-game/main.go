70707809c209fa20378735df0e5960c89099a6a6
mustafa <mustafamcm9@gmail.com>
2023-09-04 18:38:58 +0300

    create timer   Timer is created with waitgroup and goroutines.

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -6,23 +6,44 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 )
 
 
 func main() {
+	
 	csvPtr := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
 	limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds")
-
 	flag.Parse()
 
 	csv := *csvPtr
 	data, err := ReadCsvFile(csv)
-
 	if err != nil {
 		panic(err)
 	}
-	limit := *limitPtr
-	fmt.Println("limit:", limit)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		processInput(data, &wg)
+	}()
+
+	timer := time.NewTimer(time.Duration(*limitPtr) * time.Second)
+	
+	go func() {
+		<-timer.C
+		fmt.Println("Time is out")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+
+func processInput(data [][]string, wg *sync.WaitGroup) {
+	defer wg.Done() // Close the waitgroup when the repl loop is over
 
 	var questionNumber int = 1
 	var pointer int = 0
@@ -33,6 +54,7 @@ func main() {
 	questionNumberString := strconv.Itoa(questionNumber)
 	printPrompt("Quesion #"+ questionNumberString + " "+ data[pointer][0])
 	// Begin the repl loop
+
 	for reader.Scan() {
 		if pointer == len(data) - 1 { // Stop the loop if user answers all questions correctly
 			fmt.Println("You scored", examLength, "out of", examLength, "questions.")
@@ -51,8 +73,9 @@ func main() {
 		pointer += 1
 		questionNumber += 1 
 		printPrompt("Quesion #"+ strconv.Itoa(questionNumber) + " "+ data[pointer][0])
+
 	}
+	
 	// Print an additional line if we encountered an EOF character
 	fmt.Println()
-	
 }
