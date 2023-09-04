20ac500191b19635e1ad53b09e7e5aeaa1d2b151
mustafa <mustafamcm9@gmail.com>
2023-09-04 19:32:47 +0300

    add score printing when time is out   Create correctAnswerCount variable and pass its reference to process input function. With this commit, we can see users score when time is ran out.

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,81 +1,40 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"os"
 	"strconv"
 	"sync"
 	"time"
 )
 
-
-func main() {
-	
+func main() {	
 	csvPtr := flag.String("csv", "problem.csv", "a csv file in the format of 'question,answer'")
 	limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds")
-	flag.Parse()
+	flag.Parse() // Parse the flags
 
-	csv := *csvPtr
-	data, err := ReadCsvFile(csv)
+	data, err := ReadCsvFile(*csvPtr) // Read the csv file
 	if err != nil {
 		panic(err)
 	}
+	// Create a waitgroup
+	// This is for waiting till all the goroutines are done
+	var wg sync.WaitGroup 
+	var correctAnswerCount = 0
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		processInput(data, &wg)
+	wg.Add(1) // Start the waitgroup
+	go func() {	// Create a goroutine to start the repl loop
+		processInput(data, &wg, &correctAnswerCount)  
 	}()
 
-	timer := time.NewTimer(time.Duration(*limitPtr) * time.Second)
-	
-	go func() {
+	timer := time.NewTimer(time.Duration(*limitPtr) * time.Second) // Create timer with limit flag
+	go func() { // Create a goroutine to fire the timer
 		<-timer.C
-		fmt.Println("Time is out")
-		wg.Done()
+		// Time is out
+		fmt.Println("You scored", strconv.Itoa(correctAnswerCount), "out of", strconv.Itoa(len(data)), "questions.")
+		wg.Done() // Close the waitgroup
 	}()
-
+	// Wait until all goroutines are over
 	wg.Wait()
 }
 
-
-func processInput(data [][]string, wg *sync.WaitGroup) {
-	defer wg.Done() // Close the waitgroup when the repl loop is over
-
-	var questionNumber int = 1
-	var pointer int = 0
-	var correctAnswerCount = 0
-	var examLength string = strconv.Itoa(len(data))
-	reader := bufio.NewScanner(os.Stdin)
-	
-	questionNumberString := strconv.Itoa(questionNumber)
-	printPrompt("Quesion #"+ questionNumberString + " "+ data[pointer][0])
-	// Begin the repl loop
-
-	for reader.Scan() {
-		if pointer == len(data) - 1 { // Stop the loop if user answers all questions correctly
-			fmt.Println("You scored", examLength, "out of", examLength, "questions.")
-			return
-		}
-		text := cleanInput(reader.Text()) // Get the input
-		answer := data[pointer][1] 
-		// Check if answer is correct
-		if text != answer {
-			// Answer is not correct 
-			fmt.Println("You scored", strconv.Itoa(correctAnswerCount), "out of", examLength, "questions.")
-			return
-		}
-		// Answer is correct
-		correctAnswerCount += 1
-		pointer += 1
-		questionNumber += 1 
-		printPrompt("Quesion #"+ strconv.Itoa(questionNumber) + " "+ data[pointer][0])
-
-	}
-	
-	// Print an additional line if we encountered an EOF character
-	fmt.Println()
-}
