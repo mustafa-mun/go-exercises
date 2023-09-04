ee1ee6a6b7a07171849b4b384602ad35f5d48f59
mustafa <mustafamcm9@gmail.com>
2023-09-04 19:38:33 +0300

    update processInput function   Wait for user to answer all questions

--- quiz-game/process.go
+++ quiz-game/process.go
@@ -14,7 +14,6 @@ func processInput(data [][]string, wg *sync.WaitGroup, correctAnswerCount *int) 
 	
 	var questionNumber int = 1
 	var pointer int = 0
-	var examLength string = strconv.Itoa(len(data))
 	var reader *bufio.Scanner = bufio.NewScanner(os.Stdin)
 
 	questionNumberString := strconv.Itoa(questionNumber)
@@ -22,24 +21,21 @@ func processInput(data [][]string, wg *sync.WaitGroup, correctAnswerCount *int) 
 	// Begin the repl loop
 
 	for reader.Scan() {
-		if pointer == len(data) - 1 { // Stop the loop if user answers all questions correctly
-			fmt.Println("You scored", examLength, "out of", examLength, "questions.")
+		if pointer == len(data) - 1 { // Stop the loop when user is answered all questions
+			fmt.Println("You scored", strconv.Itoa(*correctAnswerCount), "out of", strconv.Itoa(len(data)), "questions.")
 			return
 		}
 		text := cleanInput(reader.Text()) // Get the input
 		answer := data[pointer][1] 
 		// Check if answer is correct
-		if text != answer {
-			// Answer is not correct 
-			fmt.Println("You scored", strconv.Itoa(*correctAnswerCount), "out of", examLength, "questions.")
-			return
+		if text == answer {
+			// Answer is correct
+			*correctAnswerCount += 1
 		}
-		// Answer is correct
-		*correctAnswerCount += 1
+		// Move to the next question
 		pointer += 1
 		questionNumber += 1 
 		printPrompt("Quesion #"+ strconv.Itoa(questionNumber) + " "+ data[pointer][0])
-
 	}
 	
 	// Print an additional line if we encountered an EOF character
