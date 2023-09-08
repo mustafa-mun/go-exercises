9db6fb422f3f35db4fbd088c7b5edf99df25b1af
mustafa <mustafamcm9@gmail.com>
2023-09-08 10:33:27 +0300

    Create json decoding types

--- cyoa/main.go
+++ cyoa/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type Story map[string]Arc
+
+type Arc struct {
+	Title string `json:"title"`
+	Story []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc string `json:"arc"`
+}
+
 func main()  {
 	fmt.Println("Hello World")
 }
