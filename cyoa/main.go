4b71815616b99e34af19929eb60e51781447cd5b
mustafa <mustafamcm9@gmail.com>
2023-09-08 18:49:28 +0300

    render the html template

--- cyoa/main.go
+++ cyoa/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"text/template"
 )
 
 type Story map[string]Arc
@@ -18,13 +19,30 @@ type Option struct {
 }
 
 func main()  {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
+
+	srv := &http.Server{
+		Addr: ":8080",
+    Handler:	mux,
+	}
+	
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic("Error: " + err.Error())
+	}
+	
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
 	jsonFile, err := openJSON("gopher.json")
 	if err != nil {
-		panic(err)
+    panic("Error: " + err.Error())
 	}
 	story, err := parseJSONToStory(jsonFile)
 	if err != nil {
-		panic(err)
+		panic("Error: " + err.Error())
 	}
-	fmt.Println(story)
+	tmpl := template.Must(template.ParseFiles("story.html"))
+	tmpl.Execute(w, story["intro"])
 }
