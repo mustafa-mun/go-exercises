f022926c858f70dab267688a5751cbdabcb6e5dc
mustafa <mustafamcm9@gmail.com>
2023-09-08 19:05:43 +0300

    Add chapter routing

--- cyoa/main.go
+++ cyoa/main.go
@@ -8,41 +8,56 @@ import (
 type Story map[string]Arc
 
 type Arc struct {
-	Title string `json:"title"`
-	Story []string `json:"story"`
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
 	Options []Option `json:"options"`
 }
 
 type Option struct {
 	Text string `json:"text"`
-	Arc string `json:"arc"`
+	Arc  string `json:"arc"`
 }
 
-func main()  {
+func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
 
 	srv := &http.Server{
-		Addr: ":8080",
-    Handler:	mux,
+		Addr:    ":8080",
+		Handler: mux,
 	}
-	
+
 	err := srv.ListenAndServe()
 	if err != nil {
 		panic("Error: " + err.Error())
 	}
-	
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	jsonFile, err := openJSON("gopher.json")
 	if err != nil {
-    panic("Error: " + err.Error())
+		panic("Error: " + err.Error())
 	}
 	story, err := parseJSONToStory(jsonFile)
 	if err != nil {
 		panic("Error: " + err.Error())
 	}
+
+	var chapter string
 	tmpl := template.Must(template.ParseFiles("story.html"))
-	tmpl.Execute(w, story["intro"])
+	chapter = r.URL.Path[1:]
+	if chapter == "" {
+		chapter = "intro"
+	}
+	
+	_, ok := story[chapter]
+	if ok {
+		// Path(chapter) found in map
+		tmpl.Execute(w, story[chapter])
+	} else {
+		// Path(chapter) not found
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Chapter not found."))
+	}
 }
