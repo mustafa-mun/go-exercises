5786a6671fa5cea2a5c06d7d01234f15daf63266
mustafa <mustafamcm9@gmail.com>
2023-09-11 20:32:45 +0300

    Rename function

--- html-link-parser/main.go
+++ html-link-parser/main.go
@@ -14,6 +14,17 @@ type Link struct {
 	Text string
 }
 
+func readHtmlFromFile(fileName string) (string, error) {
+	bs, err := os.ReadFile(fileName)
+
+	if err != nil {
+			return "", err
+	}
+
+	return string(bs), nil
+}
+
+
 func main() {
 	htmlString, err := readHtmlFromFile("ex4.html")
 	if err != nil {
@@ -29,7 +40,6 @@ func main() {
 		fmt.Println("Href: " + value.Href + " Text:" + value.Text)
 	}
 }
-
 // traverseHTMLLinks function will traverse the whole HTML
 // to find the a tags, then it will take their href and text
 // and crete Links, then it will return an array of Links
@@ -38,7 +48,7 @@ func traverseHTMLLinks(n *html.Node, linkArray *[]Link) {
 		for _, a := range n.Attr { // loop through the attributes of a tag to find the href
 			if a.Key == "href" { // We found the href
 				var linkText string
-				traverseLinkChildren(n.FirstChild, &linkText) // Take the text from a tag
+				traverseLinkChilds(n.FirstChild, &linkText) // Take the text from a tag
 				newLink := Link{ // Create new Link
 					Href: a.Val,
 					Text: linkText,
@@ -56,25 +66,15 @@ func traverseHTMLLinks(n *html.Node, linkArray *[]Link) {
 
 // traverseLinkChildren function will traverse all of the 
 // children of an a tag and extract the text 
-func traverseLinkChildren(n *html.Node, linkText *string) string { 
+func traverseLinkChilds(n *html.Node, linkText *string) string { 
 	if n != nil {
 		if n.Type != html.ElementNode && n.Type != html.CommentNode{ // Take the text only if node is not an element node or comment
 			*linkText = *linkText + " "+ strings.TrimSpace(n.Data) // Add text to the link text
 		}
 		if n.FirstChild != nil { // If node has children, traverse the childs
-			traverseLinkChildren(n.FirstChild, linkText)
+			traverseLinkChilds(n.FirstChild, linkText)
 		}
-		traverseLinkChildren(n.NextSibling, linkText) // Traverse the nextsibling
+		traverseLinkChilds(n.NextSibling, linkText) // Traverse the nextsibling
 	}
 	return *linkText
-}
-
-func readHtmlFromFile(fileName string) (string, error) {
-	bs, err := os.ReadFile(fileName)
-
-	if err != nil {
-			return "", err
-	}
-
-	return string(bs), nil
 }
