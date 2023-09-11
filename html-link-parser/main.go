2022efa09e2804532adcfd02f47a60429ba66801
mustafa <mustafamcm9@gmail.com>
2023-09-11 19:00:43 +0300

    add traverse html function   This function will traverse all html nodes, and print the a tag's text and href.

--- html-link-parser/main.go
+++ html-link-parser/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	htmlString, err := readHtmlFromFile("ex1.html")
+	if err != nil {
+		panic(err)
+	}
+	doc, err := html.Parse(strings.NewReader(htmlString))
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	traverseHTMLLinks(doc)
+}
+
+func traverseHTMLLinks(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				fmt.Println(a.Val) // Href
+				break
+			}		
+	}
+	fmt.Println(n.FirstChild.Data) // Text
+}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseHTMLLinks(c)
+	}
+}
+
+func readHtmlFromFile(fileName string) (string, error) {
+	bs, err := os.ReadFile(fileName)
+
+	if err != nil {
+			return "", err
+	}
+
+	return string(bs), nil
 }
