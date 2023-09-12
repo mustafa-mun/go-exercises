1e3cf6bd8493b000a94baa9281d77e380ea08cce
mustafa <mustafamcm9@gmail.com>
2023-09-12 16:30:25 +0300

    sitemap-builder | Add error return values to API

--- sitemap-builder/pkg/sitemap/sitemap.go
+++ sitemap-builder/pkg/sitemap/sitemap.go
@@ -11,27 +11,26 @@ type Sitemap struct{}
 
 // fetchURL will fetch an URL and return a
 // pointer to http Response
-func fetchURL(url string) *http.Response {
-	return nil
+func fetchURL(url string) (*http.Response, error) {
+	return nil, nil
 }
 
 // getResponseHTML will take a pointer to HTML
 // Response and return the HTML as a string
-func getResponseHTML(resp *http.Response) string {
-	return ""
+func getResponseHTML(resp *http.Response) (string, error) {
+	return "", nil
 }
 
 // getLinks will take a HTML string as an input
 // and return an output of all links as an array
 // of Link's
-func getLinks(html string) []linkparser.Link {
-	return nil
+func getLinks(html string) ([]linkparser.Link, error) {
+	return nil, nil
 }
 
 // traverseLinks will take a link as an input
 // and traverse the whole link path recursively
-// or iteratively (DFS or BFS) then return an
-// array of Sitemaps as an output
+// then return an array of Sitemaps as an output
 func traverseLinks(link string) []Sitemap {
 	return nil
 }
@@ -39,6 +38,6 @@ func traverseLinks(link string) []Sitemap {
 // CreateSitemap will fetch the base domain and
 // create a sitemap with all links under the same
 // domain and return encoded XML with the sitemaps
-func CreateSitemap(baseURL string) []byte {
-	return nil
+func CreateSitemap(baseURL string) ([]byte, error) {
+	return nil, nil
 }
