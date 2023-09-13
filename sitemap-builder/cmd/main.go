3e1ba821e969813ec6d69a02918162f9ff9ca155
mustafa <mustafamcm9@gmail.com>
2023-09-14 01:13:32 +0300

    sitemap-builder | Update cmd main   Accept URL from a flag

--- sitemap-builder/cmd/main.go
+++ sitemap-builder/cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/mustafa-mun/go-exercises/sitemap-builder/pkg/sitemap"
 )
 
 func main()  {
-	stmp, err := sitemap.CreateSitemap("https://recipe-share-cmv8.onrender.com")
+	urlPtr := flag.String("url", "https://gobyexample.com/", "Base URL for sitemap builder")
+	flag.Parse() 
+
+	stmp, err := sitemap.CreateSitemap(*urlPtr)
 	if err != nil{
 		panic(err)
 	}	
