c4528b246368c30d8025eba02398b3c08cdb6d4b
mustafa <mustafamcm9@gmail.com>
2023-09-13 21:13:13 +0300

    sitemap-builder | Return xml as an output   Currently, this function works but its extremely slow, will be optimized.

--- sitemap-builder/cmd/main.go
+++ sitemap-builder/cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"os"
+
 	"github.com/mustafa-mun/go-exercises/sitemap-builder/pkg/sitemap"
 )
 
 func main()  {
-	_, err := sitemap.CreateSitemap("https://www.calhoun.io/")
+	stmp, err := sitemap.CreateSitemap("https://recipe-share-cmv8.onrender.com")
 	if err != nil{
 		panic(err)
 	}	
+	err = os.WriteFile("sitemap.xml", stmp, 0644)
+	if err != nil{
+		panic(err)
+	}	
+	
 }
--- sitemap-builder/pkg/sitemap/sitemap.go
+++ sitemap-builder/pkg/sitemap/sitemap.go
@@ -1,17 +1,34 @@
 package sitemap
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/mustafa-mun/go-exercises/html-link-parser/pkg/linkparser"
 )
 
 // XML Sitemap structure (for decoding XML)
-type Sitemap struct{}
+type Url struct{
+	XMLName xml.Name `xml:"url"`
+	Loc string `xml:"loc"`
+}
 
 
+func GetURLHost(url string) (string, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+	return response.Request.URL.Host, nil
+}
+func IsUrl(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
 // getResponseHTML will will fetch an URL and 
 // return the response HTML as a string
 func getResponseHTML(url string) (string, error) {
@@ -19,6 +36,7 @@ func getResponseHTML(url string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	defer response.Body.Close()
 	htmlBytes, err := io.ReadAll(response.Body)
 	if err != nil {
@@ -31,39 +49,58 @@ func getResponseHTML(url string) (string, error) {
 // traverseLinks will take a link as an input
 // and traverse the whole link path recursively
 // then return an array of Sitemaps as an output
-func traverseLinks(link, baseURL string, seen map[string]bool) error{
-	_, ok := seen[link]
-	if ok { // If link is seen, return
-		return nil
+func traverseLinks(link, baseURL string, seen map[string]bool, sitemapArray *[]string) (*[]string, error){
+	fmt.Println(link)
+	if seen[link] {
+		return sitemapArray, nil
+	} else {
+		seen[link] = true
 	}
 
-	linkHost,err := GetURLHost(link)
-	if err != nil{
-		return err
+	newUrl := Url{ // Create new XML URL
+		Loc: link,
 	}
-	baseHost, err := GetURLHost(baseURL)
-	if err != nil{
-		return err
-	}
+	*sitemapArray = append(*sitemapArray, newUrl.Loc)
 
-	if linkHost != baseHost { // If link host is not same with baseURL, return
-		return nil
-	}
-	
 	htmlString, err := getResponseHTML(link)
 	if err != nil{
-		return err
+		return nil, err
 	}
 	linkArray, err := linkparser.Parse(htmlString)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	fmt.Println(linkArray)
+
 	for _, link := range linkArray{
-		seen[link.Href] = true // Add link to seen list
-		traverseLinks(link.Href, baseURL, seen)
+		var target string 
+	
+		if !IsUrl(link.Href){
+			if link.Href == "/" {
+				target = baseURL
+			} else {
+				target = baseURL + link.Href 
+			}
+			
+		} else {
+			target = link.Href
+		}
+
+		linkHost, err := GetURLHost(target)
+		if err != nil{	
+			return nil, err
+		}
+
+		baseHost, err := GetURLHost(baseURL)
+		if err != nil{
+			return nil, err
+		}
+		if linkHost == baseHost && !seen[target] { // If link host is not same with baseURL, return
+			traverseLinks(target, baseURL, seen, sitemapArray)
+		}
+		
 	}
-	return nil
+	
+	return sitemapArray, nil
 }
 
 // CreateSitemap will fetch the base domain and
@@ -71,18 +108,15 @@ func traverseLinks(link, baseURL string, seen map[string]bool) error{
 // domain and return encoded XML with the sitemaps
 func CreateSitemap(baseURL string) ([]byte, error) {
 	seen := make(map[string]bool)
-	err := traverseLinks(baseURL, baseURL, seen)
+	var sitemapArray []string
+	_, err := traverseLinks(baseURL, baseURL, seen, &sitemapArray)
 	if err != nil{
 		return nil, err
 	}
-	return nil, nil
-}
-
-func GetURLHost(url string) (string, error) {
-	response, err := http.Get(url)
+	out, err := xml.MarshalIndent(sitemapArray, "", " ")
+	
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	defer response.Body.Close()
-	return response.Request.URL.Host, nil
+	return out, nil
 }
