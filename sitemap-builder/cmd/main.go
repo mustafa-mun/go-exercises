64d58fdddb68e23aea4122b4d01012137977162b
mustafa <mustafamcm9@gmail.com>
2023-09-14 14:36:57 +0300

    sitemap-builder | Adjust default depth

--- sitemap-builder/cmd/main.go
+++ sitemap-builder/cmd/main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	urlPtr := flag.String("url", "https://gobyexample.com/", "Base URL for sitemap builder")
-	depthPtr := flag.Int("depth", 5, "defines the maximum number of links to follow")
+	depthPtr := flag.Int("depth", 9, "defines the maximum number of links to follow")
 	flag.Parse()
 
 	stmp, err := sitemap.CreateSitemap(*urlPtr, *depthPtr)
 	if err != nil {
 		panic(err)
 	}
-	err = os.WriteFile("sitemap.xml", stmp, 0644)
+	err = os.WriteFile("sitemap.xml", stmp, 0644) // Write the sitemap XML to a file
 	if err != nil {
 		panic(err)
 	}
